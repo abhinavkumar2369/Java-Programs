864ca4c145590c66c6e403af623e09d84d83c5f9
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-06-11 20:39:05 +0530

    Added ➕

---
+++ Index Of.java
@@ -0,0 +1,16 @@
+public class MyClass{
+  public static void main(String[] args){
+
+    // Declaring Word
+    String word = "abcdefghijklmnopqrstuvwxyz";
+
+    
+    // ------------- Index -------------
+    
+    System.out.println(word.indexOf("cd"));
+    // 2
+    
+    System.out.println(word.indexOf("az"));
+    // -1
+  }
+}
