d1fdc5e9c0886739114dc6ff8abfa8efe1c92935
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-06-11 20:41:56 +0530

    Added ➕

---
+++ String Built-in method.java
@@ -0,0 +1,25 @@
+public class MyClass{
+  public static void main(String[] args){
+
+    // Declaring Sentence
+    String sentence = "abcdefghijklmnopqrstuvwxyz";
+
+    // Printing Length of Sentence
+    System.out.println(sentence.length());
+
+    // Printing Sentence
+    System.out.println(sentence);
+
+    // Printing uppercase using return value
+    System.out.println(sentence.toUpperCase());
+
+    // Printing Sentence
+    System.out.println(sentence);
+
+    // changing the value of the sentence
+    sentence = sentence.toUpperCase();
+
+    // Printing Sentence
+    System.out.println(sentence);
+  }
+}
